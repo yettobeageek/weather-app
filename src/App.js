4c61efde2eeb4173a7d1945d52eea58452082be5
yettobeageek <manojgowdago@gmail.com>
2022-06-24 13:20:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   
   return (
     <div className={(typeof weather.main!="undefined")?
-    ((weather.main.temp>16)?'app warm': 'app')  :'app'
+    ((weather.main.temp>25)?'app warm': 'app')  :'app'
   }>
       <main>
         <div className="search-box">
